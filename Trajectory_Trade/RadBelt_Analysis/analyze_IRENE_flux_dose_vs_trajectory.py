1bce21a1fb18a5d3608d9917695e4d667deac709
amdestefano <61249904+anthony-destefano@users.noreply.github.com>
2021-07-08 14:15:41 -0500

    Update analyze_IRENE_flux_dose_vs_trajectory.py

--- Trajectory_Trade/RadBelt_Analysis/analyze_IRENE_flux_dose_vs_trajectory.py
+++ Trajectory_Trade/RadBelt_Analysis/analyze_IRENE_flux_dose_vs_trajectory.py
@@ -4,9 +4,9 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-traj_filename      = 'ephem_sat.dat'
-diff_flux_filename = 'Equator_DifferentialFlux_Electron_Proton.txt'
-dose_rate_filename = 'Equator_DoseRate_Electron_Proton_Total.txt'
+traj_filename      = 'ephem_sat2.dat'
+diff_flux_filename = 'Equator2_DifferentialFlux_Electron_Proton.txt'
+dose_rate_filename = 'Equator2_DoseRate_Electron_Proton_Total.txt'
 
 
 
@@ -55,9 +55,26 @@ traj_r = np.sqrt(traj_x**2 + traj_y**2 + traj_z**2)
 flux_data = np.loadtxt(diff_flux_filename, unpack=True)
 
 flux_time     = flux_data[0,:]
-flux_electron = flux_data[1:NE_e,:]
+flux_electron = flux_data[1:NE_e+1,:]
 flux_proton   = flux_data[NE_e+1:,:]
 
-plt.semilogy(traj_r, flux_electron.T)
+# Read dose rate file
+
+dose_data = np.loadtxt(dose_rate_filename, unpack=True)
+
+dose_time     = dose_data[0,:]
+dose_electron = dose_data[1:N_d+1,:]
+dose_proton   = dose_data[N_d+1:2*N_d+1,:]
+dose_total    = dose_data[2*N_d+1:,:]
+
+
+plt.loglog(traj_r[::2], dose_electron.T)
+plt.figure()
+plt.loglog(traj_r[::2], dose_proton.T)
 plt.show()
+
+# plt.loglog(traj_r, flux_electron.T)
+# plt.figure()
+# plt.loglog(traj_r, flux_proton.T)
+# plt.show()
 
